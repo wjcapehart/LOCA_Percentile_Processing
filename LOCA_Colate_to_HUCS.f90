022eb8e86f4d7f5b5525c425d99e9a016bee379c
William J. Capehart <William.Capehart@sdsmt.edu>
2019-08-20 19:50:35 -0600

    Update LOCA_Colate_to_HUCS.f90

--- LOCA_Colate_to_HUCS.f90
+++ LOCA_Colate_to_HUCS.f90
@@ -17,8 +17,8 @@ program LOCA_Colate_to_HUCS
   integer, parameter :: ntime_hist = 20454
   integer, parameter :: ntime_futr = 34333
 
-  integer (kind=4) :: myhuc_low   = 10120000 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
-  integer (kind=4) :: myhuc_high  = 10129999 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
+  integer (kind=4) :: myhuc_low   = 10160000 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
+  integer (kind=4) :: myhuc_high  = 10169999 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
 
   integer (kind=4), allocatable          :: start_t(:)
   integer (kind=4), allocatable          :: end_t(:)
