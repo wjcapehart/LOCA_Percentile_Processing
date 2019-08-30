fc6398c9c6fcdff087a09f66e0387c7840581ef7
William J. Capehart <William.Capehart@sdsmt.edu>
2019-08-30 10:40:39 -0600

    Update LOCA_Colate_to_HUCS.f90

--- LOCA_Colate_to_HUCS.f90
+++ LOCA_Colate_to_HUCS.f90
@@ -25,8 +25,8 @@ program LOCA_Colate_to_HUCS
 
   integer (kind=4) :: t_buffer
 
-  integer (kind=4) :: myhuc_low    = 10160000 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
-  integer (kind=4) :: myhuc_high   = 10169999 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
+  integer (kind=4) :: myhuc_low    = 10140000 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
+  integer (kind=4) :: myhuc_high   = 10149999 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
 
   integer (kind=4), allocatable          :: start_t(:)
   integer (kind=4), allocatable          :: end_t(:)
