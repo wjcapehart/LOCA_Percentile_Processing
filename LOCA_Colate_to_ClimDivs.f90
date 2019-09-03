b3423da9a7aa5a41146cad8dc93ae7d871be118e
William J. Capehart <William.Capehart@sdsmt.edu>
2019-09-02 18:08:35 -0600

    correcting a truncation line error

--- LOCA_Colate_to_ClimDivs.f90
+++ LOCA_Colate_to_ClimDivs.f90
@@ -24,8 +24,10 @@ program LOCA_Colate_to_ClimDivs
   character (len=*), PARAMETER  :: map_variable_name = "US_CAN_Zones"
   character (len=*), PARAMETER  :: map_values_name   = "US_CAN_Zones_ID"
   character (len=*), PARAMETER  :: filename_map      = "./USCAN_Climate_Divisions.nc"
-  character (len=*), PARAMETER  :: file_front_root =  "/maelstrom2/LOCA_GRIDDED_ENSEMBLES/LOCA_NGP/"
-  character (len=*), PARAMETER  :: file_output_root = "/maelstrom2/LOCA_GRIDDED_ENSEMBLES/LOCA_NGP/climate_divisions/NGP_LOCA_nCLIMDIV_"
+  character (len=*), PARAMETER  :: file_front_root   = "/maelstrom2/LOCA_GRIDDED_ENSEMBLES/LOCA_NGP/"
+  character (len=*), PARAMETER  :: file_output_root  = &
+                    "/maelstrom2/LOCA_GRIDDED_ENSEMBLES/LOCA_NGP/climate_divisions/NGP_LOCA_nCLIMDIV_"
+
 
   integer (kind=4) :: myhuc_low    = 3201 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
   integer (kind=4) :: myhuc_high   = 3201 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
