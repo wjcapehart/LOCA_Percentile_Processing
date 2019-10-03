185b52a510036aeb4ebf87fe7d21125f5435b6e6
William J. Capehart <William.Capehart@sdsmt.edu>
2019-10-02 19:53:29 -0600

    Finishing California and Colorado

--- LOCA_Colate_to_ClimDivs.f90
+++ LOCA_Colate_to_ClimDivs.f90
@@ -24,11 +24,11 @@ program LOCA_Colate_to_ClimDivs
   character (len=*), PARAMETER  :: file_output_root  = &
                     "/maelstrom2/LOCA_GRIDDED_ENSEMBLES/LOCA_NGP/climate_divisions/NGP_LOCA_nCLIMDIV_"
 
-  integer, parameter :: start_scen = 2
-  integer, parameter :: end_scen   = 2
+  integer, parameter :: start_scen = 3
+  integer, parameter :: end_scen   = nscen
 
-  integer (kind=4) :: myhuc_low    = 3901 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
-  integer (kind=4) :: myhuc_high   = 3999 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
+  integer (kind=4) :: myhuc_low    = 0401 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
+  integer (kind=4) :: myhuc_high   = 0599 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
 
   integer, parameter :: npull = 90    ! 2, 3, 7, 487
 
