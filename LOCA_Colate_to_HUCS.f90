6bac43e66bc7ddded5da11de9020bec538b67f6f
William J. Capehart <William.Capehart@sdsmt.edu>
2019-10-02 19:14:05 -0600

    Niobrara Deployed

--- LOCA_Colate_to_HUCS.f90
+++ LOCA_Colate_to_HUCS.f90
@@ -20,8 +20,8 @@ program LOCA_Colate_to_HUCS
   integer, parameter :: start_scen = 2
   integer, parameter :: end_scen   = nscen
 
-  integer (kind=4) :: myhuc_low    = 10170000 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
-  integer (kind=4) :: myhuc_high   = 10179999 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
+  integer (kind=4) :: myhuc_low    = 10150000 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
+  integer (kind=4) :: myhuc_high   = 10159999 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
 
   character (len=*), PARAMETER  :: map_variable_name = "HUC08_Code"
   character (len=*), PARAMETER  :: map_values_name   = "HUC08_Code_ID"
