64e65c5013eff19a0adb6c85d67880e4d73d667f
William J. Capehart <William.Capehart@sdsmt.edu>
2019-10-09 11:22:21 -0600

    Adding Offline Frequcny

--- LOCA_HUC_Extreme_Event_Analysis_Offline_Loop.R
+++ LOCA_HUC_Extreme_Event_Analysis_Offline_Loop.R
@@ -32,7 +32,6 @@
 
   remove(HUC_AVAIL_URL)
 
-bigger_table =
 
 # Select Periods
 
@@ -40,7 +39,7 @@ start_years    = c(1951, 1961, 1976, 2011, 2036, 2061, 2070, 1956, 2006, 2026, 2
 period_length  = c(  30,  30,    30,   30,   30,   30,   30,   50,   50,   50,  50)
 end_years      = start_years + period_length -1
 
-
+number_of_periods = length(start_years)
 
 
 center_years = 0.5 * (end_years + start_years) - 0.5
@@ -49,7 +48,7 @@ center_years
 Periods = tibble(start_years   = start_years,
                  center_years  = center_years,
                  end_years     = end_years,
-                 period_length = period_length)
+                 period_length = period_length) 
 
 Periods_filename = str_c("/projects/ECEP/LOCA_MACA_Ensembles/LOCA/LOCA_ExtRemes/HUC08/",
                       "NGP_LOCA_HUC08_",
@@ -58,6 +57,7 @@ Periods_filename = str_c("/projects/ECEP/LOCA_MACA_Ensembles/LOCA/LOCA_ExtRemes/
 print(Periods)
 
 save(Periods, file=Periods_filename)
+remove(Periods)
 
 
 
@@ -76,7 +76,7 @@ for (huc_zone_lut in Completed_HUCS)
     filter(HUC08_Code_ID == huc_zone_lut)
 
   root_LOCA_URL = "http://kyrill.ias.sdsmt.edu:8080/thredds/fileServer/LOCA_NGP/huc_08_regions/"
-  root_LOCA_URL = "/maelstrom2/LOCA_GRIDDED_ENSEMBLES/LOCA_NGP/huc_08_basins/"
+ # root_LOCA_URL = "/maelstrom2/LOCA_GRIDDED_ENSEMBLES/LOCA_NGP/huc_08_basins/"
 
   loca_filename = str_c("NGP_LOCA_HUC08_",
                         huc_zone_lut,
@@ -101,10 +101,14 @@ for (huc_zone_lut in Completed_HUCS)
 
 
 # Period Extraction (using the zone maximum daily event)
-  for (start_year in start_years)
+  for (i in seq(from = 1, 
+                to   = number_of_periods,
+                by   = 1))
   { # Start Year
+    
+    start_year = Periods$start_years[i]
+    end_year = Periods$end_years[i]
 
-    end_year = start_year + 29
 
 
 
@@ -174,6 +178,7 @@ for (huc_zone_lut in Completed_HUCS)
                                    start_year         = start_year,
                                    center_year        = 0.5 * (start_year + end_year) - 0.5,
                                    end_year           = end_year,
+                                   period_length      = Periods$period_length[i],
                                    Ensemble           = ensemble,
                                    Return_Period      = year_return,
                                    Return_Estimate_05 = return_ci[,1],
@@ -187,6 +192,7 @@ for (huc_zone_lut in Completed_HUCS)
                                    start_year         = start_year,
                                    center_year        = 0.5 * (start_year + end_year) - 0.5,
                                    end_year           = end_year,
+                                   period_length      = Periods$period_length[i],
                                    Ensemble           = ensemble,
                                    Return_Period      = year_return,
                                    Return_Estimate_05 = return_ci[,1],
