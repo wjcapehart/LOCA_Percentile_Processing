66dfb9976db5e8ba0d50c0620bec6b7c466b0e74
William J. Capehart <William.Capehart@sdsmt.edu>
2019-10-30 09:04:31 -0600

    LowerMO

--- LOCA_Colate_to_HUCS.f90
+++ LOCA_Colate_to_HUCS.f90
@@ -22,8 +22,8 @@ program LOCA_Colate_to_HUCS
   integer, parameter :: start_scen = 1
   integer, parameter :: end_scen   = nscen
 
-  integer (kind=4) :: myhuc_low    = 10210007 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
-  integer (kind=4) :: myhuc_high   = 10210010 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
+  integer (kind=4) :: myhuc_low    = 10300001 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
+  integer (kind=4) :: myhuc_high   = 10309999 ! 10170000 (Big Sioux) !  10120000 (Chey)  !  10160000 (James)
 
   character (len=*), PARAMETER  :: map_variable_name = "HUC08_Code"
   character (len=*), PARAMETER  :: map_values_name   = "HUC08_Code_ID"
