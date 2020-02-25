b49b5c3bffdeeaffe161592caa24a86f598ba6c3
William J. Capehart <William.Capehart@sdsmt.edu>
2020-02-24 21:33:57 -0700

    Update LOCA_Colate_to_Lucas.f90

--- LOCA_Colate_to_Lucas.f90
+++ LOCA_Colate_to_Lucas.f90
@@ -27,10 +27,10 @@ program LOCA_Colate_to_ClimDivs
   integer, parameter :: start_scen = 1
   integer, parameter :: end_scen   = nscen
 
-  integer (kind=4) :: myhuc_low    = 22
+  integer (kind=4) :: myhuc_low    = 23
   integer (kind=4) :: myhuc_high
 
-  integer, parameter :: npull = 365    ! 2, 3, 7, 487 
+  integer, parameter :: npull = 365    ! 2, 3, 7, 487
 
   integer (kind=4) :: t_buffer
 
