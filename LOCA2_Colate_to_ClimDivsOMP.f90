928a80b457d3b7ee799d72bb095ac46546850784
Bill Capehart <William.Capehart@sdsmt.edu>
2023-07-20 14:52:07 -0600

    Update LOCA2_Colate_to_ClimDivsOMP.f90

--- LOCA2_Colate_to_ClimDivsOMP.f90
+++ LOCA2_Colate_to_ClimDivsOMP.f90
@@ -46,7 +46,7 @@ program LOCA_Colate_to_ClimDivs
   integer, parameter :: end_scen   = nscen
 
   integer (kind=4) :: myhuc_low    = 3905
-  integer (kind=4) :: myhuc_high   = 3906
+  integer (kind=4) :: myhuc_high   = 3905
 
   integer, parameter :: npull =  50 !, 3, 7, 487
 
