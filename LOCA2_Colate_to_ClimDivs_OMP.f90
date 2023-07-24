8065df19a9b989fa8f560ab21db523b6ac0448b3
Bill Capehart <William.Capehart@sdsmt.edu>
2023-07-24 13:21:42 -0400

    x

--- LOCA2_Colate_to_ClimDivs_OMP.f90
+++ LOCA2_Colate_to_ClimDivs_OMP.f90
@@ -704,7 +704,7 @@ program LOCA_Colate_to_ClimDivs
             do jj = 1, nlat
               do ii = 1, nlon
 
-                write(6,"('A,x,I4,I4,I4,I8,x,F10.1,F10.1x,F10.1)")
+                write(6,'(A,x,I4,I4,I4,I8,x,F10.1,F10.1x,F10.1)')
                     "   h,i,j,t,map_pr,map_tmax,huc_map:", &
                              myhucs(h), &
                              ii,jj,t, &
