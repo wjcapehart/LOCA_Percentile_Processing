67f6b65ec2db40f17dec19efa33453c513171810
Bill Capehart <William.Capehart@sdsmt.edu>
2024-01-14 11:48:57 -0700

    new polygons

--- LOCA2_Colate_to_ClimDivs.f90
+++ LOCA2_Colate_to_ClimDivs.f90
@@ -47,8 +47,8 @@ program LOCA_Colate_to_ClimDivs
   integer, parameter :: start_scen = 1
   integer, parameter :: end_scen   = nscen
 
-  integer (kind=4) :: myhuc_low    = 3904
-  integer (kind=4) :: myhuc_high   = 3904
+  integer (kind=4) :: myhuc_low    = 3905
+  integer (kind=4) :: myhuc_high   = 3908
 
   integer, parameter :: npull = 365! 2 !, 3, 7, 487
 
