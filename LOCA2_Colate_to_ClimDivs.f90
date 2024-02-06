cebc0a9af8aceddca2f19e1a26f349586d0ccb80
Bill Capehart <William.Capehart@sdsmt.edu>
2024-02-05 20:31:20 -0700

    3105

--- LOCA2_Colate_to_ClimDivs.f90
+++ LOCA2_Colate_to_ClimDivs.f90
@@ -45,8 +45,8 @@ program LOCA_Colate_to_ClimDivs
   integer, parameter :: start_scen = 1
   integer, parameter :: end_scen   = nscen
 
-  integer (kind=4) :: myhuc_low    = 3104
-  integer (kind=4) :: myhuc_high   = 3104
+  integer (kind=4) :: myhuc_low    = 3105
+  integer (kind=4) :: myhuc_high   = 3105
 
   integer, parameter :: npull = 365! 2 !, 3, 7, 487
 
