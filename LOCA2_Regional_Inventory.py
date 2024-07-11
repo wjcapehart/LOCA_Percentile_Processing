192f2ecacbafead079d80012f2aad81aa195b44f
Bill Capehart <William.Capehart@sdsmt.edu>
2024-07-11 11:33:59 -0600

    More Output Text for Inventory Python Script

--- LOCA2_Regional_Inventory.py
+++ LOCA2_Regional_Inventory.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## Inventory for LOCA2 Regions
+# # THREDDS Servier Inventory for LOCA2 Aggregated Regions
 
 # In[ ]:
 
+
+####################################################
+#
+# Libraries
+#
+
+print("Libraries")
 
 import numpy              as np
 import cartopy.crs        as ccrs
@@ -20,22 +27,25 @@ import geopandas          as gp
 
 from   siphon.catalog import TDSCatalog
 
-
 import socket
-print(socket.gethostname())
 
-myhostname = socket.gethostname()
+print("----")
 
+#
+####################################################
+
+
+# ## Creating SD Mines Branded Colors
 
 # In[ ]:
 
 
 ####################################################
-####################################################
-####################################################
 #
 # Mines Colors and Fonts
 #
+
+print("Mines Colors and Fonts for Local Branding")
 
 Mines_Blue = "#002554"
 
@@ -47,18 +57,13 @@ plt.rcParams.update({'text.color'      : Mines_Blue,
 					 'ytick.color'     : Mines_Blue,
                      'font.family'     : "Open Sans"})
 
+print("----")
 
 #
 ####################################################
-####################################################
-####################################################
 
 
-# In[ ]:
-
-
-
-
+# ## Determine What Machine we are working on.
 
 # In[ ]:
 
@@ -67,6 +72,12 @@ plt.rcParams.update({'text.color'      : Mines_Blue,
 #
 # Kyrill vs Mandrenke
 #
+
+print("Begin Script: Identify Machine")
+
+print(socket.gethostname())
+
+myhostname = socket.gethostname()
 
 if ("mandrenke" in socket.gethostname()): # Mandrenke
     target_dir  = "/Users/wjc/GitHub/LOCA_Percentile_Processing/"
@@ -77,24 +88,23 @@ else:  # Kyrill
 
 print(target_dir)
 
+print("----")
+
 #
 ####################################################
 
 
-# In[ ]:
-
-
-
-
+# ## Pulling Inventories from the THREDDS Server
 
 # In[ ]:
 
 
+####################################################
+#
+# Pulling Available RData Inventories from THREDDS Server 
+#
 
-
-
-# In[ ]:
-
+print("Pulling Inventory from THREDDS Server")
 
 dir_hucs = "http://kyrill.ias.sdsmt.edu:8080/thredds/catalog/LOCA2/Specific_Regional_Aggregate_Sets/USGS_HUC08_Basins/R_Annual_Files/catalog.xml"
 dir_cdiv = "http://kyrill.ias.sdsmt.edu:8080/thredds/catalog/LOCA2/Specific_Regional_Aggregate_Sets/NCEI_Climate_Divisions/R_Annual_Files/catalog.xml"
@@ -122,23 +132,35 @@ table_cdiv = pd.read_csv(filepath_or_buffer = dir_cdiv_csv)
 available_hucs = np.array(available_hucs, dtype=np.int32)
 available_cdiv = np.array(available_cdiv, dtype=np.int32)
 
+print("----")
 
+#
+####################################################
+
+
+# ## Cleaning the HUC-08 Inventories
 
 # In[ ]:
 
 
+####################################################
+#
+# Cleaning Available HUC Inventory
+#
+
+print("Cleaning USGS HUC 08 Inventory")
+
 table_huc = table_hucs[table_hucs["huc08"].isin(available_hucs)]
 
-
 table_huc = table_huc[['huc08',
-                         'huc08_name', 
-                         'huc06_name', 
-                         'huc04_name', 
-                         'huc02_name']].rename(columns = {"huc08"     : "HUC-08",
-                                                          "huc08_name": "BASIN NAME",
-                                                          "huc06_name": "HUC-06 BASIN",
-                                                          "huc04_name": "HUC-04 BASIN",
-                                                          "huc02_name": "HUC-02 REGION"})
+                       'huc08_name', 
+                       'huc06_name', 
+                       'huc04_name', 
+                       'huc02_name']].rename(columns = {"huc08"     : "HUC-08",
+                                                        "huc08_name": "BASIN NAME",
+                                                        "huc06_name": "HUC-06 BASIN",
+                                                        "huc04_name": "HUC-04 BASIN",
+                                                        "huc02_name": "HUC-02 REGION"})
 
 table_huc["HUC-08"] = [str(x).zfill(8)   for x in table_huc["HUC-08"].values]
 table_huc = table_huc.sort_values("HUC-08")
@@ -151,21 +173,36 @@ if (display_img) :
 
 os.system('csvtotable --caption "Available HUC-08 Files" --overwrite ' +  target_dir + "HUC_table_avail.csv " + target_dir + "HUC_table_avail.html")
 
+print("----")
 
+#
+####################################################
+
+
+# ## Cleaning the Climate Division Inventories
 
 # In[ ]:
 
 
+####################################################
+#
+# Cleaning Available ClimDiv Inventory
+#
+
+print("Cleaning NCEI ClimDiv Inventory")
+
 table_div = table_cdiv[table_cdiv["climdiv"].isin(available_cdiv)][["climdiv",
-                                                                   "climdiv_name",
-                                                                   "climdiv_state_name"]]
+                                                                    "climdiv_name",
+                                                                    "climdiv_state_name"]]
 
 table_div["climdiv"] = [str(x).zfill(4) for x in table_div["climdiv"].values]
+
 table_div  = table_div.rename(columns = {"climdiv":"DIVISION",
-                           "climdiv_name":"NAME",
-                           "climdiv_state_name":"STATE"}).sort_values("DIVISION")
+                                         "climdiv_name":"NAME",
+                                          "climdiv_state_name":"STATE"}).sort_values("DIVISION")
 
 table_div.to_csv(path_or_buf = target_dir + "ClimDiv_table_avail.csv", index=False)
+
 print(target_dir + "ClimDiv_table_avail.csv")
 
 if (display_img) :
@@ -174,92 +211,109 @@ if (display_img) :
 os.system('csvtotable --caption "Available ClimDiv Files" --overwrite ' +  target_dir + "ClimDiv_table_avail.csv " + target_dir + "ClimDiv_table_avail.html")
 
 
+print("----")
+
+#
+####################################################
+
+
+# ## Pulling GeoJSON Polygon Files
+
 # In[ ]:
 
 
+####################################################
+#
+# Cleaning Available ClimDiv Inventory
+#
 
-
-
-# In[ ]:
-
+print("Read the GeoJSON Polygon Files")
 
 shp_hucs = gp.read_file("GeoJSON_Files/CONUS_USGS_HUC-08.geojson")
 shp_cdiv = gp.read_file("GeoJSON_Files/CONUS_NCEI_Climate_Divisions.geojson")
 
+print("Initial Polygon Elements")
+print("       HUC08:RAW:",len(shp_hucs), len(shp_hucs.get_coordinates()))
+print("     CLIMDIV:RAW:",len(shp_cdiv), len(shp_cdiv.get_coordinates()))
+
+print("----")
+
+#
+####################################################
+
+
+# ## Retain only Polygons Associated with an Available RData File
 
 # In[ ]:
 
 
-print("  HUC08:RAW:",len(shp_hucs), len(shp_hucs.get_coordinates()))
-print("CLIMDIV:RAW:",len(shp_cdiv), len(shp_cdiv.get_coordinates()))
+####################################################
+#
+# Retain only Polygons Associated with an Available RData File
+#
 
-
-# In[ ]:
-
+print("Retain only Polygons Associated with an Available RData File")
 
 shp_cdiv = shp_cdiv[shp_cdiv["CLIMDIV"].isin(available_cdiv)]
 shp_hucs = shp_hucs[shp_hucs[  "huc8" ].isin(available_hucs)]
 
+print("  Available Polygon Elements")
+print("       HUC08:CUT:",len(shp_hucs), len(shp_hucs.get_coordinates()))
+print("     CLIMDIV:CUT:",len(shp_cdiv), len(shp_cdiv.get_coordinates()))
 
+#
+# Plot Climate Divisions if in a Jupyter Session
+#
 
 if (display_img) :
     display(shp_cdiv)
+    display(shp_hucs)
+
+print("----")
+
+#
+####################################################
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+# ## Dissolve Available Polygons
 
 # In[ ]:
 
 
+####################################################
+#
+# Dissolve Available Polygons
+#
 
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
+print("Dissolve All Available Polygons")
 
 shp_cdiv = shp_cdiv.assign(CLIMDIV=1) 
 shp_hucs = shp_hucs.assign(   huc8=1) 
-print("  HUC08:CUT:",len(shp_hucs), len(shp_hucs.get_coordinates()))
-print("CLIMDIV:CUT:",len(shp_cdiv), len(shp_cdiv.get_coordinates()))
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 shp_cdivD = shp_cdiv.dissolve(by = "CLIMDIV")
 shp_hucsD = shp_hucs.dissolve(by =    "huc8")
 
+print("  Available Polygon Elements")
+print("       HUC08:DIS:",len(shp_hucsD), len(shp_hucs.get_coordinates()))
+print("     CLIMDIV:DIS:",len(shp_cdivD), len(shp_cdiv.get_coordinates()))
+
+print("----")
+
+#
+####################################################
+
+
+# ## Produce HUC Image
 
 # In[ ]:
 
 
-print("  HUC08:DIS:",len(shp_hucsD), len(shp_hucs.get_coordinates()))
-print("CLIMDIV:DIS:",len(shp_cdivD), len(shp_cdiv.get_coordinates()))
+####################################################
+#
+# Produce HUC Image
+#
 
-
-# In[ ]:
+print("Produce HUC Image")
 
 
 ccrs_proj = ccrs.AlbersEqualArea(central_longitude  =   -96, 
@@ -274,6 +328,7 @@ ax  = fig.add_subplot(1, 1, 1,
 
 #fig.suptitle("CMIP6 LOCA-2 Available Regions", color = Mines_Blue, fontsize= 'xx-large')
 #ax.set_title("USGS HUC-08 Basins", color = Mines_Blue, fontsize= 'x-large')
+
 ax.set_extent([-119, -73, 22.5, 51])
 
 ax.add_feature(cfeature.COASTLINE, edgecolor = Mines_Blue)
@@ -283,8 +338,6 @@ ax.add_feature(cfeature.STATES,    edgecolor = Mines_Blue, linewidth=0.5)
 
 
 #ax.set_frame_on(False)
-
-
 
 shp_hucs.plot(ax        = ax,
               aspect    = 'equal',
@@ -298,9 +351,27 @@ plt.savefig(target_dir + "./LOCA2_HUCs_Available_Regions_Map.png")
 if (display_img) :
     plt.show()
 
+print("----")
+
+#
+####################################################
+
 
 # In[ ]:
 
+
+
+
+
+# ## Produce ClimDiv Image
+
+# In[ ]:
+
+
+####################################################
+#
+# Produce ClimDiv Image
+#
 
 ccrs_proj = ccrs.AlbersEqualArea(central_longitude  =   -96, 
                                  central_latitude   =  37.5, 
@@ -321,9 +392,7 @@ ax.add_feature(cfeature.LAKES,     facecolor = 'None')
 #ax.add_feature(cfeature.RIVERS,    edgecolor = Mines_Blue)
 ax.add_feature(cfeature.STATES,    edgecolor = Mines_Blue, linewidth=0.5)
 
-
 #ax.set_frame_on(False)
-
 
 shp_cdiv.plot(ax        = ax,
               aspect    = 'equal',
@@ -337,10 +406,24 @@ plt.tight_layout()
 plt.savefig(target_dir + "./LOCA2_ClimDiv_Available_Regions_Map.png")
 if (display_img) :
     plt.show()
+    
+print("----")
 
+#
+####################################################
+
+
+# ## Produce Combined ClimDiv + HUC Image
 
 # In[ ]:
 
+
+####################################################
+#
+# Produce Combined ClimDiv + HUC Image
+#
+
+print("Produce Combined ClimDiv + HUC Image")
 
 ccrs_proj = ccrs.AlbersEqualArea(central_longitude  =   -96, 
                                  central_latitude   =  37.5, 
@@ -361,9 +444,7 @@ ax.add_feature(cfeature.LAKES,     facecolor = 'None')
 #ax.add_feature(cfeature.RIVERS,    edgecolor = Mines_Blue)
 ax.add_feature(cfeature.STATES,    edgecolor = Mines_Blue, linewidth=0.5)
 
-
 #ax.set_frame_on(False)
-
 
 shp_cdiv.plot(ax        = ax,
               aspect    = 'equal',
@@ -385,21 +466,8 @@ plt.savefig(target_dir + "./LOCA2_Available_Regions_Map.png")
 if (display_img) :
     plt.show()
 
+print("----")
 
-# In[ ]:
-
-
-
-
-
-# In[1]:
-
-
-
-
-
-# In[ ]:
-
-
-
+#
+####################################################
 
